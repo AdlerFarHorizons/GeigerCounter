ebc8665ad535e3fda387adf5b3b673288576802c
lnigra <lounigra@gmail.com>
2014-07-01 17:12:06 -0500

    Major improvements: timer interrupt; better flow; better SD use.

--- software/GeigerCounterExternal/GeigerCounterExternal.ino
+++ software/GeigerCounterExternal/GeigerCounterExternal.ino
@@ -1,68 +1,133 @@
-/*
-  Adapted from CosmicRayCounter V2.1 software
+/*  
+  A compatible logic level pulsed detector output from the Geiger counter is
+  required and connected to Pin 3 (INT1) configured for rising edge interrupt
+  trigger.
+  
+  SD Card file is created or appended if the card is present.
+  
+  An optional LED between Pin 9 and GND indicates presence of valid SD card.
+ 
+  An optional LM60 temperature sensor with pins connected to 5V, A0, GND in
+  order from left to right, flat side facing you.
 */
 
 #include <MsTimer2.h>
 #include <SD.h>
 
 const int chipSelect = 10;
+const int ledPin = 9;
+const int vTempPin = 0;
+const float vRef = 5.0;
 
 long deltaT;
 volatile int crCount;
 File dataFile;
-long int time;
+float time;
+volatile boolean active;
+const String filename = "geigerex.txt";
 
 void setup() {
+  
   deltaT = 5000;
   Serial.begin(9600);
   Serial.flush();
+  
   Serial.print("Initializing SD card...");
-  //make sure that the default chip select pin is set to
-  //output, even if you don't use it:
+
+  // SD library requires Pin 10 as output, even if not used as CS:
   pinMode(10, OUTPUT);
+  analogWrite( ledPin, 50 );
+  //delay( 1000 );
   
-  // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
+    
+    Serial.println("Card failed or not present.");
+    analogWrite( ledPin, 0 );
+    
+  } else {
+    
+    Serial.println("card initialized.");
+    analogWrite( ledPin, 50 );
+    
   }
-  Serial.println("card initialized.");
-  dataFile = SD.open( "datalog.txt", FILE_WRITE );
-  dataFile.println( "\n==== Counter Started =====" );
-  dataFile.println( "time window start (ms), counts in time window" );
-  dataFile.flush(); 
   
-  attachInterrupt(1, increment, RISING);
+  dataFile = SD.open( "datalog.txt", FILE_WRITE );
+
+  Serial.println( "\n==== Counter Started =====\n" );
+  Serial.print( "Count Window:" );Serial.print( deltaT / 1000.0, 3 );
+  Serial.print( " sec\n" );
+  Serial.flush(); 
+
+  if ( dataFile ) {
+    dataFile.println( "\n==== Counter Started =====\n" );
+    dataFile.print( "Count Window:," );dataFile.print( deltaT / 1000.0, 3 );
+    dataFile.println( ",sec\n" );
+    dataFile.println( "Window Start (sec), Counts, T (C)" );
+    dataFile.flush();
+  }
+
+  MsTimer2::set( deltaT, timesUp );
+  MsTimer2::start();
+  active = true;
+  crCount = 0;
+  attachInterrupt( 1, increment, RISING );
+  time = 0;
+  
 }
 
-void loop()
-{  
-  time = millis();
+void loop() {
+  
+  while( active );
+  detachInterrupt( 1 );
+  long tempCount = crCount;
   crCount = 0;
-  delay( deltaT );
-  Serial.println( crCount );
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  attachInterrupt( 1, increment, RISING );
+  active = true;
 
-  // if the file is available, write to it:
+  Serial.print( time, 3 );Serial.print( " sec: " );
+  Serial.print( tempCount );Serial.print( " counts " );;
+  Serial.print( readTemp( vTempPin, 0 ) );Serial.println( " deg C");
+  
   if (dataFile) {
     
-    //dataFile.println(dataString);
-    dataFile.print( time );dataFile.print( ",");dataFile.println( crCount );
-    dataFile.close();
-    // print to the serial port too:   
-
+    dataFile.print( time, 3 );dataFile.print( ",");
+    dataFile.print( tempCount );dataFile.print( ",");
+    dataFile.println( readTemp( vTempPin, 0 ) );
+    dataFile.flush();
+    
   }  
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
-
+  
+  time += deltaT / 1000.0;
   
 }
 
-void increment()
-{
+void increment() {
+  
   crCount++;
+  
+}
+
+void timesUp() {
+  
+  active = false;
+  
+}
+
+float readTemp( int pin, int sensType ) {
+  // Temperature Sensor constants:
+  //   0  LM60
+  //   1  MAX6605
+  //   2  TMP36
+  int mVoltsAtRefTemp[] = { 424, 744, 750 };
+  int refTempC[] = { 0, 0, 25 };
+  float mVperDegC[] = { 6.25, 11.9, 10.0 };
+
+  int reading = analogRead(vTempPin);
+  float mVolts = reading * vRef / 1.024;
+
+  return( ( mVolts - mVoltsAtRefTemp[sensType] ) / 
+            ( mVperDegC[sensType] ) + 
+            refTempC[sensType]);
+  
 }
 
